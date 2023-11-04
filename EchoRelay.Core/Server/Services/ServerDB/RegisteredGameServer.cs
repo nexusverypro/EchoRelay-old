dd5232df7da8e9ff6019316c4875a93e89405b50
Luka Grenon <lukagrenon@icloud.com>
2023-11-04 17:36:07 -0400

    meow
    wtf man

--- EchoRelay.Core/Server/Services/ServerDB/RegisteredGameServer.cs
+++ EchoRelay.Core/Server/Services/ServerDB/RegisteredGameServer.cs
@@ -513,8 +513,8 @@ namespace EchoRelay.Core.Server.Services.ServerDB
         public override string ToString()
         {
             var peers = GetPlayers().Result;
-            return string.Format("GameServer(SessionId={0], Channel={1}, LobbyType={2}, Peers={3})", SessionId, SessionChannel, SessionLobbyType, string.Format("[{0}]", string.Join(", ", (from x in peers
-                                                                                                                                                                                            select x.Peer.Id))));
+            return string.Format("GameServer(SessionId={0}, Channel={1}, LobbyType={2}, Peers={3})", SessionId, SessionChannel, SessionLobbyType, string.Format("[{0}]", string.Join(", ", (from x in peers
+                                                                                                                                                                                            select x.Peer.UserDisplayName))));
         }
         #endregion
     }
