1c9563fbf55b4317d5f373b50e89d900ef520fe1
Luka Grenon <lukagrenon@icloud.com>
2023-11-04 15:57:44 -0400

    Create BanPlayerCommand.cs

---
+++ EchoRelay.CLI/ConsoleCommands/Custom/BanPlayerCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EchoRelay.CLI.ConsoleCommands.Custom
+{
+    internal class BanPlayerCommand : CommandBase
+    {
+        public override string Name => "ban";
+        public override string Description => "Bans a player from your server. [args: ('id' any possible) OR ('name' any possible)]";
+
+        
+    }
+}
