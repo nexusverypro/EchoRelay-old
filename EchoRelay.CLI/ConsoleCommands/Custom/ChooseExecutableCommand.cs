0fd174838822a393165335d4f9409d1b09e11a2b
Luka Grenon <lukagrenon@icloud.com>
2023-11-05 10:07:44 -0500

    Added command for selecting new executable
    set new executable

---
+++ EchoRelay.CLI/ConsoleCommands/Custom/ChooseExecutableCommand.cs
@@ -0,0 +1,23 @@
+﻿using EchoRelay.Core.ConsoleUtils;
+using EchoRelay.Core.Server.Storage.Filesystem;
+using EchoRelay.Core.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EchoRelay.CLI.ConsoleCommands.Custom
+{
+    internal class ChooseExecutableCommand : CommandBase
+    {
+        public override string Name => "setexec";
+        public override string Description => "Set a new executable as the Echo VR executable, no args needed.";
+
+        public override async Task Execute(CommandArguments args)
+        {
+            ConsoleLogger.LogMessage(LogType.Info, "Test Command successfully executed!");
+            await Program.SetNewExecutable();
+        }
+    }
+}
--- EchoRelay.CLI/Program.cs
+++ EchoRelay.CLI/Program.cs
@@ -4,11 +4,39 @@ using EchoRelay.Core.ConsoleUtils;
 using System;
 using System.Security.Cryptography;
 using System.Windows.Forms;
+using static EchoRelay.Core.Server.Storage.Types.AccountResource.AccountClientProfile;
 
 namespace EchoRelay.CLI
 {
     public static class Program
     {
+
+        public static async Task SetNewExecutable()
+        {
+            var appSettings = AppSettings.Load(Constants.SettingsFile);
+            ConsoleLogger.s_DisableWriteToConsole = true;
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Title = "Select Echo Arena executable",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "txt",
+                Filter = "Executable Files (*.exe)|*.exe",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                appSettings!.GameExecutableFilePath = openFileDialog.FileName;
+                appSettings.Save(Constants.SettingsFile);
+            }
+        }
         private static async Task Main(string[] args)
         {
             Console.BufferWidth = (int)(Console.BufferWidth * 1.2f);
